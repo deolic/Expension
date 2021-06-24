ecd1ee2c9f0f3304e66f18df6912544ad75bbcd7
deolic <piotrsidorowicz@outlook.com>
2021-06-25 01:09:14 +0200

    initial commit

---
+++ Controllers/UserController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Expension.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UserController : ControllerBase
+    {
+        // GET: api/user
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/values/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/values
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/values/5
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/values/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
